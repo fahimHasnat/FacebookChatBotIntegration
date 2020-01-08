f5dba6ad16bb153b008210fcc7875e1a0fb25d13
Fahim <fahim.hasnat@v2.ltd>
2020-01-08 16:09:53 +0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.send("Hi I am a chatbot");
+})
+
+app.listen(app.get('port'),()=>{
+    console.log('Server is listening');
+});

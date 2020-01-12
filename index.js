acf6f7c018154e8808c8da5a0c4a0bea7c6816f0
Fahim <fahim.hasnat@v2.ltd>
2020-01-12 14:28:03 +0600

    Second Commit

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/', (req, res)=>{
     res.send("Hello World");
 });
 
+app.get('/webhook/',(req, res)=>{
+    if(req.query['hub.verify_token'] === "balblabla"){
+        res.send(req.query['hub.challenge']);
+    }
+    res.send("Wrong Token");
+})
 app.listen(app.get('port'),()=>{
     console.log('Server is listening');
 });
